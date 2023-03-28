da50d35e9c385a82e4bed721aab9ed8ee00441ff
Prince Oncada <prince.oncada@gmail.com>
2023-03-28 19:25:50 +0800

    tkinter grid system

--- GUI.py
+++ GUI.py
@@ -2,7 +2,13 @@ from tkinter import *
 
 root = Tk()
 
-myLabel = Label(root, text='Hello World!')
-myLabel.pack()
+myLabel1 = Label(root, text='Hello World!')
+myLabel2 = Label(root, text='My Name is Prince Oncada!')
+myLabel3 = Label(root, text='               ')
+
+myLabel1.grid(row=0, column=0)
+myLabel2.grid(row=1, column=5)
+myLabel3.grid(row=1, column=1)
+
 
 root.mainloop()
