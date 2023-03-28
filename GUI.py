bcee43a23970c959daeba344c0e17a3d33e95085
Prince Oncada <prince.oncada@gmail.com>
2023-03-28 22:27:48 +0800

    calculator app with other arithmetic operations

--- GUI.py
+++ GUI.py
@@ -8,20 +8,70 @@ e.grid(row=0, column=0, columnspan=3, padx=10, pady=10)
 
 def button_click(number):
     e.insert(END, number)
+    
+def button_clear():
+    e.delete(0, END)
+    
+def btn_add():
+    first_number = e.get()
+    global f_num
+    global math 
+    math = "+"
+    f_num = int(first_number)
+    e.delete(0, END)
+    
+def btn_subtract():
+    first_number = e.get()
+    global f_num
+    global math 
+    math = "-"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+def btn_multiply():
+    first_number = e.get()
+    global f_num
+    global math 
+    math = "*"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+def btn_divide():
+    first_number = e.get()
+    global f_num
+    global math 
+    math = "/"
+    f_num = int(first_number)
+    e.delete(0, END)
+
+def btn_equal():
+    second_number = int(e.get())
+    e.delete(0, END)
+    e.insert(0, eval(f"{f_num}{math}{second_number}"))
+    
 
 button_1 = Button(root, text="1", padx=40, pady=20, command=lambda: button_click(1))
 button_2 = Button(root, text="2", padx=40, pady=20, command=lambda: button_click(2))
 button_3 = Button(root, text="3", padx=40, pady=20, command=lambda: button_click(3))
+
 button_4 = Button(root, text="4", padx=40, pady=20, command=lambda: button_click(4))
 button_5 = Button(root, text="5", padx=40, pady=20, command=lambda: button_click(5))
 button_6 = Button(root, text="6", padx=40, pady=20, command=lambda: button_click(6))
+
 button_7 = Button(root, text="7", padx=40, pady=20, command=lambda: button_click(7))
 button_8 = Button(root, text="8", padx=40, pady=20, command=lambda: button_click(8))
 button_9 = Button(root, text="9", padx=40, pady=20, command=lambda: button_click(9))
+
 button_0 = Button(root, text="0", padx=40, pady=20, command=lambda: button_click(0))
-button_add = Button(root, text="+", padx=39, pady=20, command=lambda: button_click("+"))
-button_equal = Button(root, text="-", padx=90, pady=20, command=lambda: button_click("-"))
-button_clear = Button(root, text="Clear", padx=79, pady=20, command=lambda: button_click(" "))
+
+button_add = Button(root, text="+", padx=39, pady=20, command=btn_add)
+
+button_subtract = Button(root, text="-", padx=40, pady=20, command=btn_subtract)
+button_multiply = Button(root, text="*", padx=40, pady=20, command=btn_multiply)
+button_divide = Button(root, text="/", padx=40, pady=20, command=btn_divide)
+
+button_equal = Button(root, text="=", padx=90, pady=20, command=btn_equal)
+button_clear = Button(root, text="Clear", padx=79, pady=20, command=button_clear)
 
 button_1.grid(row=3, column=0)
 button_2.grid(row=3, column=1)
@@ -36,9 +86,14 @@ button_8.grid(row=1, column=1)
 button_9.grid(row=1, column=2)
 
 button_0.grid(row=4, column=0)
-button_clear.grid(row=4, column=1, columnspan=2)
+
 button_add.grid(row=5, column=0)
+button_subtract.grid(row=6, column=0)
+button_multiply.grid(row=6, column=1)
+button_divide.grid(row=6, column=2)
+
 button_equal.grid(row=5, column=1, columnspan=2)
+button_clear.grid(row=4, column=1, columnspan=2)
 
 def myClick():
     hello = f"Hello {e.get()}"
