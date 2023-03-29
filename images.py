0ab26a8292b0ef72de3306c1e96547b341690708
Prince Oncada <prince.oncada@gmail.com>
2023-03-29 09:52:45 +0800

    adding a status bar for image viewer

--- images.py
+++ images.py
@@ -3,17 +3,64 @@ from PIL import ImageTk, Image
 
 root = Tk()
 root.title('Icons')
-root.iconbitmap('mcme.ico')
+root.iconbitmap('images/mcme.ico')
 
+cur_img = 0
 
+img1 = ImageTk.PhotoImage(Image.open("images/1.png").resize((500, 500)))
+img2 = ImageTk.PhotoImage(Image.open("images/2.png").resize((500, 500)))
+img3 = ImageTk.PhotoImage(Image.open("images/3.png").resize((500, 500)))
+img4 = ImageTk.PhotoImage(Image.open("images/4.png").resize((500, 500)))
+img5 = ImageTk.PhotoImage(Image.open("images/5.png").resize((500, 500)))
 
-my_img = ImageTk.PhotoImage(Image.open("me.png").resize((300, 300)))
-myLabel = Label(image=my_img,)
-myLabel.pack()
+image_list = [img1, img2, img3, img4, img5]
 
+my_label = Label(image=image_list[cur_img])
+my_label.grid(row=0, column=0, columnspan=3)
 
+def check():
+    global cur_img
+    
+    status = Label(root, text=f"Image {cur_img+1} of {len(image_list)}", bd=1, relief=SUNKEN, anchor=E, padx=10, pady=5)
+    
+    if(cur_img == 0):
+        button_back = Button(root, text="<<", command=lambda: back(), state=DISABLED)
+    else:
+        button_back = Button(root, text="<<", command=lambda: back())
+        
+    if(cur_img == 4):
+        button_forward = Button(root, text=">>", command=lambda: forward(), state=DISABLED)
+    else:
+        button_forward = Button(root, text=">>", command=lambda: forward())
+        
+    button_back.grid(row=1, column=0)
+    button_forward.grid(row=1, column=2, pady=10)
+    status.grid(row=2, column=0, columnspan=3, sticky=W+E)
+    
+def forward():
+    global my_label
+    global cur_img
+    
+    if cur_img < 4:
+        cur_img = cur_img + 1
+        my_label.grid_forget()
+        my_label = Label(image=image_list[cur_img])
+        my_label.grid(row=0, column=0, columnspan=3)
+        check()
 
-btn_quit = Button(root, text="Exit Program", command=root.quit)
-btn_quit.pack()
+def back():
+    global my_label
+    global cur_img
+    
+    if cur_img > 0:
+        cur_img = cur_img - 1
+        my_label.grid_forget()
+        my_label = Label(image=image_list[cur_img])
+        my_label.grid(row=0, column=0, columnspan=3)
+        check()
+
+button_quit = Button(root, text="Exit Program", command=root.quit)
+button_quit.grid(row=1, column=1)
+check()
 
 root.mainloop()
